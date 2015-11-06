36d42fb276cb3cda9a36af1168269acf09a4f24f
alu0100831696 <alu0100831696@ull.edu.es>
2015-11-06 02:09:22 +0000

    Última confirmación

--- bib/README.md
+++ bib/README.md
@@ -1,3 +1,10 @@
+Lenguajes Paradigmas de la Programación
+Práctica Desarrollo dirigido por pruebas (TDD) #6
+
+Andrea Pérez Quintana
+Sofía Pizarro Arbelo
+
+
 # Bib
 
 Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/bib`. To experiment with that code, run `bin/console` for an interactive prompt.
--- bib/spec/bib_spec.rb
+++ bib/spec/bib_spec.rb
@@ -1,6 +1,60 @@
+#Andrea Pérez Quintana
+
 require 'spec_helper'
 require 'bib'
 
 describe Bib do
- it 'requires additional testing'
+ 
+ #it 'requires additional testing'
+ 
+ before :each do
+       
+    autor = [ 'Dave Thomas', 'Andy Hunt', 'Chad Fowler' ]
+    titulo = 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers Guide'
+    serie = '(The Facets of Ruby)'
+    editorial = 'Pragmatic Bookshelf'
+    edicion = '4 edition'
+    fecha = 'July 7, 2013'
+    isbn = [ 'ISBN-13: 978-1937785499', 'ISBN-10: 1937785491']
+ 
+ @object1 = Bib.new(autor, titulo, serie, editorial, edicion, fecha, isbn) #Se crea el primer objeto
+ 
+  end
+  
+  describe "# Object" do
+    it "Creación de objeto" do
+      @object1.should be_an_instance_of Bib
+    end
+  end
+  
+  describe "# Debe existir" do
+   
+    it "Uno o más autores" do
+        @object1.get_autor.length.should_not be 0  # Longitud de Autor no debe ser cero.
+    end
+   
+    it "Un título" do
+      @object1.get_titulo.length.should_not be 0  
+    end
+   
+    it "O no una serie" do
+      @object1.get_serie.should_not be 'V'  # V = vacío.
+    end
+    
+    it "Una editorial" do
+      @object1.get_serie.length.should_not be 0
+    end
+   
+    it "Una edición" do
+      @object1.get_edicion.length.should_not be 0
+    end
+   
+    it "Una fecha" do
+      @object1.get_fecha.length.should_not be 0
+    end
+   
+    it "Uno o más ISBN" do
+      @object1.get_isbn.length.should_not be 0 
+    end
+  end
 end
